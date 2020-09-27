40fdc51ddc41d07a046f56246a92a9f417967def
Eivind Moe Hammersmark <eivindhammers@gmail.com>
2020-09-27 11:26:28 +0200

    fix

--- get_scores.R
+++ get_scores.R
@@ -2,12 +2,10 @@ library(blastula)
 library(glue)
 library(plyr)
 library(dplyr)
-library(fplscrapR)
-
-api_key <- source("api-key.R")[[1]]
+library(fplscrapR) #remotes::install_github("wiscostret/fplscrapR")
 
 # Read previous scores
-scores_prev <- read.csv2("scores.csv", stringsAsFactors = FALSE)
+scores_prev <- read.csv2("scores_20-21.csv", stringsAsFactors = FALSE)
 
 matches <- get_game_list() %>%
   #filter(finished == TRUE) %>%
@@ -32,40 +30,40 @@ write.csv2(matches, "scores_20-21.csv", row.names = FALSE)
 scores_diff <- anti_join(matches, scores_prev)
 
 # source("create_creds.R")
-
-if (nrow(scores_diff) > 0) {
-  results_txt <- scores_diff %>%
-    mutate(result_string = paste0(team, "  ", team_h_score, " - ", team_a_score, "  ", opponents)) %>%
-    pull(result_string) %>%
-    paste(collapse = "<br>")
-  
-  current_date_time <- add_readable_time(use_tz = TRUE)
-  
-  # Generate the body text for the email message
-  email_text <- 
-  glue(
-  "
-  Hei,
-  
-  Premier League-resultatene har blitt oppdatert med følgende kamper:
-  
-  {results_txt}
-  
-  Mvh PL-roboten
-  "
-  )
-  
-  email_object <-
-    compose_email(
-      body = md(email_text),
-      footer = glue("Email sent on {current_date_time}.")
-    )
-  
-  email_object %>%
-    smtp_send(
-      to = "emh@osloeconomics.no",
-      from = "eivindhammers@gmail.com",
-      subject = "Oppdatering av Premier League-resultater",
-      credentials = creds_file("gmail_creds")
-    )
-}
+# 
+# if (nrow(scores_diff) > 0) {
+#   results_txt <- scores_diff %>%
+#     mutate(result_string = paste0(team, "  ", team_h_score, " - ", team_a_score, "  ", opponents)) %>%
+#     pull(result_string) %>%
+#     paste(collapse = "<br>")
+#   
+#   current_date_time <- add_readable_time(use_tz = TRUE)
+#   
+#   # Generate the body text for the email message
+#   email_text <- 
+#   glue(
+#   "
+#   Hei,
+#   
+#   Premier League-resultatene har blitt oppdatert med følgende kamper:
+#   
+#   {results_txt}
+#   
+#   Mvh PL-roboten
+#   "
+#   )
+#   
+#   email_object <-
+#     compose_email(
+#       body = md(email_text),
+#       footer = glue("Email sent on {current_date_time}.")
+#     )
+#   
+#   email_object %>%
+#     smtp_send(
+#       to = "emh@osloeconomics.no",
+#       from = "eivindhammers@gmail.com",
+#       subject = "Oppdatering av Premier League-resultater",
+#       credentials = creds_file("gmail_creds")
+#     )
+# }
